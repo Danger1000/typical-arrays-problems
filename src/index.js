20de8b39250d68bd552511256086cc664e10e689
Danger1000 <60090781+Danger1000@users.noreply.github.com>
2020-03-17 20:23:29 +0300

    feat: add solution of "typical-arrays-problems" task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
 exports.min = function min (array) {
-  return 0;
+    let min = 0;
+    if (array && array.length > 0) {
+        min = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] < min) min = array[i];
+        }
+    }
+    return min;
 }
-
 exports.max = function max (array) {
-  return 0;
+    let max = 0;
+    if (array && array.length > 0) {
+        max = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] > max) max = array[i];
+        }
+    }
+    return max;
 }
-
 exports.avg = function avg (array) {
-  return 0;
+    let avg = 0;
+    if (array && array.length > 0) {
+        let i = 0;
+        let counts = 0;
+        for ( i ; i < array.length; i++) {
+            avg += array[i];
+            counts++;
+        }
+        avg = avg / counts;
+    }
+    return avg;
 }
